424d7ca6dc6746d74a182c446a167b9a4feef675
Jeff Ward <jeff.ward@willowtreeapps.com>
2018-12-09 13:03:08 -0500

    Switch from scrolling tab bar to bottom tab bar.

--- lib/main.dart
+++ lib/main.dart
@@ -29,25 +29,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
-  List<Tab> _tabs = [
-    Tab(
-      text: 'Games',
-    ),
-    Tab(
-      text: 'Events',
-    ),
-    Tab(
-      text: 'Feedback',
-    )
+  List<BottomNavigationBarItem> _tabs = [
+    BottomNavigationBarItem(title: Text('Games'), icon: Icon(Icons.gamepad)),
+    BottomNavigationBarItem(title: Text('Events'), icon: Icon(Icons.event)),
+    BottomNavigationBarItem(title: Text('Feedback'), icon: Icon(Icons.feedback))
   ];
-  int _counter = 0;
+  int _currentIndex = 0;
   TabController _tabController;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   void initState() {
@@ -66,29 +54,31 @@ class _MyHomePageState extends State<MyHomePage>
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: _tabs,
-        ),
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: <Widget>[
-          _buildGameList(),
-          Center(
-            child: Text('Events'),
-          ),
-          Center(
-            child: FeedbackScreen()
-          ),
-        ],
+      bottomNavigationBar: BottomNavigationBar(
+        items: _tabs,
+        currentIndex: _currentIndex,
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _buildBody(),
     );
+  }
+
+  Widget _buildBody() {
+    switch (_currentIndex) {
+      case 0:
+        return _buildGameList();
+      case 1:
+        return Center(
+          child: Text('Events'),
+        );
+      case 2:
+        return FeedbackScreen();
+    }
   }
 
   Widget _buildGameList() {
